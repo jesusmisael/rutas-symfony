5d1351d4ae711d036f06eccc8ed3f16f25fa2141
Abraham Villarux <abraham.villaraux@gmail.com>
2021-12-06 09:36:10 -0600

    Una actualizacion

--- app/Controller/IndexController.php
+++ app/Controller/IndexController.php
@@ -8,7 +8,12 @@ use Symfony\Component\HttpFoundation\Response;
 class IndexController
 {
     public function index(){
-        $html = General::render_template('index.php','');
+        /*
+         * $argumento1 = "Esto es un ejemplo de un valor que se envia a la vista"
+         * $html = General::render_template(['index.php',[$argumento1]);
+         * $html = General::render_template(['index.php']);
+        */
+        $html = General::render_template(['index.php']);
         return new Response($html);
     }
 }
--- app/Helper/General.php
+++ app/Helper/General.php
@@ -6,17 +6,17 @@ use Symfony\Component\HttpFoundation\Response;
 
 class General
 {
-    static function render_template($path, $args)
+    static function render_template(array $args)
     {
-        if($args!=""){
-            if(is_array($args)) {
-                extract($args);
+        if($args[1]!=""){
+            if(is_array($args[1])) {
+                extract($args[1]);
             } else {
                 return new Response('Ha pasado un parametro diferente a un array', 500);
             }
         }
         ob_start();
-        require VIEW_PATH . $path;
+        require VIEW_PATH . $args[0];
         return ob_get_clean();
     }
 }
