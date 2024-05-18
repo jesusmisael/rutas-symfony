0c17e9922196758a0402bf1c7e4ac7f6504378a5
Jesús Misael Betanzos Betanzos <jesusbetanzos@outlook.com>
2024-05-18 03:07:19 -0600

    se modifico configuracion para agregar configuracion de variables para el envio de correo

--- app/config/App.php
+++ app/config/App.php
@@ -14,6 +14,10 @@ class App
         define("DB_USER", $_ENV['DB_USER']);
         define("DB_PASSWORD", $_ENV['DB_PASSWORD']);
         define("DB_NAME", $_ENV['DB_NAME']);
+        define("MAIL_HOST", $_ENV['MAIL_HOST']);
+        define("MAIL_USER", $_ENV['MAIL_USERNAME']);
+        define("MAIL_PASSWORD", $_ENV['MAIL_PASSWORD']);
+        define("MAIL_PORT", $_ENV['MAIL_PORT']);
 
         if(MODE=="production"){
             define('PATH_WS', $_ENV['PATH_WS_PRODUCTION']);
